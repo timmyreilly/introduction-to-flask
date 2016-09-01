ad1b75047d2e17e6abd7ce353ebc1cc5c928607b
TimmyReilly <timreilly@live.com>
2016-09-01 15:46:47 -0700

    added first template

--- hello.py
+++ hello.py
@@ -1,11 +1,15 @@
 #app.py 
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template('user.html', name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
