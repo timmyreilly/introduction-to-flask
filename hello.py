c500e3218e96f903aef76469147acf587b225607
TimmyReilly <timreilly@live.com>
2016-09-01 15:54:44 -0700

    working resume

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
 #app.py 
-
 from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess'
+
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-
-@app.route("/user/<name>")
-def user(name):
-    return render_template('user.html', name=name)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
