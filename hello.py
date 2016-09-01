b55c6c7b5adb6287b925351773c1d8476287fdd8
TimmyReilly <timreilly@live.com>
2016-09-01 16:19:58 -0700

    finished with working search

--- hello.py
+++ hello.py
@@ -25,9 +25,12 @@ def search():
     form = SearchForm()
     if form.validate_on_submit():
         session['searchterm'] = form.searchterm.data 
-        session['sentiment'], session['titlearticle'] = get_sentiment_from_url(session.get('searchterm'))
+        session['sentiment'], session['titlearticle'], session['wikiurl'] = get_sentiment_from_url(get_one_url_from_wiki_search(session.get('searchterm')))
         return redirect(url_for('search'))
-    return render_template('search.html', form=form, searchterm=session.get('searchterm'), number=session.get('sentiment'), titlearticle=session.get('titlearticle'))
+    return render_template('search.html', form=form, searchterm=session.get('searchterm'), number=session.get('sentiment'), titlearticle=session.get('titlearticle'), wikiUrl=session.get('wikiurl'))
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- wiki_sentiment.py
+++ wiki_sentiment.py
@@ -3,8 +3,19 @@ import requests
 import json 
 from tokens import * 
 
-def get_sentiment_from_url(url='https://en.wikipedia.org/wiki/Monty_Python'):
+def get_one_url_from_wiki_search(word='Monty Python'):
+    url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + word + "&limit=1&namespace=0&format=json"
     r = requests.get(url)
+    content = json.loads(r.content)
+    try:
+        search = str(content[3][0])
+    except:
+        return 'https://en.wikipedia.org/wiki/Monty_Python' 
+
+    return search 
+
+def get_sentiment_from_url(search_url='https://en.wikipedia.org/wiki/Monty_Python'):
+    r = requests.get(search_url)
 
     soup = BeautifulSoup(r.text, "html.parser")
     l = soup.find_all('p')
@@ -19,10 +30,7 @@ def get_sentiment_from_url(url='https://en.wikipedia.org/wiki/Monty_Python'):
     r = requests.post(url, data=json.dumps(data), headers=headers)
 
     content = json.loads(r.content)
-    return content['documents'][0]['score'], str(h[0].text) 
-
-
-
+    return content['documents'][0]['score'], str(h[0].text), search_url  
 
 
 
